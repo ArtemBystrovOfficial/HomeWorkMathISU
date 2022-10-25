460707ea8129c4e4bd05b1ac5dd06d155cbaff75
ArtemBystrovOfficial <bystrov.official.one@gmail.com>
2022-10-25 22:54:26 +0800

    Gauss fix

--- source/cpp/Gauss.cpp
+++ source/cpp/Gauss.cpp
@@ -2,31 +2,67 @@
 #include "../headers/Gauss.hpp"
 
 #include <vector>
+#include <map>
 
 std::vector<double> MethodGauss(matrix mat,std::vector<double> con)
 {
+
+    std::map <int, bool> usebles;
+
+    if (!mat.size())
+        return std::vector<double>{};
+
+    // only matrix n x n, other return answers with x1,x2...
+    if(mat.size()!=mat[0].size())
+        return std::vector<double>{};
 
     for(int i=0;i<mat.size();i++)
     {
         mat[i].push_back(con[i]);
     }
+    
+    int lvl = 0;
 
-    for(int k=0;k < mat.size();k++)
+    while (usebles.size()!=mat.size())
     {
-
-        multiply_row_scalar(mat,k,(1/mat[k][k]));   
-
-        for(int i=0;i<mat.size();i++)
+        for (int k = 0; k < mat.size(); k++)
         {
-            if(k!=i)
+            if (usebles.find(k) != usebles.end() || mat[k][lvl]==0)
+                continue;
+
+            usebles[k] = true;
+
+            multiply_row_scalar(mat, k, (1 / mat[k][lvl]));
+
+            for (int i = 0; i < mat.size(); i++)
             {
-                double kof = mat[i][k];
-                for(int j=k;j<mat[0].size();j++)
+                if (k != i)
                 {
-                    mat[i][j]-=mat[k][j]*kof;
+                    double kof = mat[i][lvl];
+                    for (int j = lvl; j < mat[0].size(); j++)
+                    {
+                        mat[i][j] -= mat[k][j] * kof;
+                    }
                 }
             }
         }
+        lvl++;
     }
-    return get_column_matrix(mat,mat[0].size());
+    
+    lvl = 0;
+
+    while (lvl != mat.size())
+    {
+        for (int i = 0; i < mat.size(); i++)
+        {
+            if (mat[i][lvl] == 1.0)
+            {
+                replace_rows_matrix(mat, lvl, i);
+                break;
+            }
+        }
+        lvl++;
+    }
+
+    return get_column_matrix(mat,mat[0].size()-1);
 }
--- source/cpp/GaussTests.cpp
+++ source/cpp/GaussTests.cpp
@@ -3,8 +3,20 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+bool operator==(const std::vector<double>& vec1, const std::vector<double>& vec2)
+{
+    if (vec1.size() != vec2.size())
+        return false;
+    for (int i = 0; i < vec1.size(); i++)
+    {
+        if (vec1[i] != vec2[i])
+            return false;
+    }
+    return true;
+}
 
-TEST(gauss,test_result)
+
+TEST(gauss,test_result1)
 {
     //A
 
@@ -17,9 +29,59 @@ TEST(gauss,test_result)
 
     //A
 
-    MethodGauss(mat,vec);
+    auto out = MethodGauss(mat,vec);
+
+    bool is = out == std::vector<double>{-2.0, 1.0, -1.0};
 
     //A
+
+    ASSERT_TRUE(is);
+
+}
+
+TEST(gauss, test_result2)
+{
+    //A
+
+    matrix mat = {{-1, 2, 6},
+                  {3, -6, 0},
+                  {1, 0, 6} };
+
+    std::vector <double> vec{ 15, -9, 5 };
+
+
+    //A
+
+    auto out = MethodGauss(mat, vec);
+
+    bool is = out == std::vector<double>{-7, -2, 2};
+
+    //A
+
+    ASSERT_TRUE(is);
+
+}
+
+TEST(gauss, test_result3)
+{
+    //A
+
+    matrix mat = { {2, 3},
+                   {4, 3} };
+
+    std::vector <double> vec{ 2, 7 };
+
+
+    //A
+
+    auto out = MethodGauss(mat, vec);
+
+    bool is = out == std::vector<double>{2.5, -1};
+
+    //A
+
+    ASSERT_TRUE(is);
+
 }
 
 int main(int argc, char **argv)
